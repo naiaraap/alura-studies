5805d0e70ab1417463c5f32fb05d3c271f91621c
naiaraap <naiarapantuza@gmail.com>
2023-02-27 12:04:26 -0300

    first commit

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,10 +1,11 @@
 import React from "react";
+import style from  './Button.module.scss';
 
-class Button extends React.Component {
+class Button extends React.Component <any, any> {
     render() {
         return(
-            <button>
-                Botão
+            <button className={style.button}>
+                {this.props.children}
             </button>
         );
     }
---
+++ src/pages/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Cronometro from '../components/Cronometro';
+import Form from '../components/Form';
+import List from '../components/List';
+import style from './App.module.scss';
+
+function App() {
+  return (
+    <div className={style.AppStyle}>
+      <Form/>
+      <List/>
+      <Cronometro/>
+    </div>
+  );
+}
+
+export default App;
